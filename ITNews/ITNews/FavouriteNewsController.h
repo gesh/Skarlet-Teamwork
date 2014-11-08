505c73dedeef44b8a9cbd416f1a836ce6be0fb93
Delyan Nikolaev Nikolov <delyan.nikolov.1992@gmail.com>
2014-11-08 14:50:45 +0200

    Added FavouritesNewsControler

--- ITNews/ITNews/FavouriteNewsController.h
+++ ITNews/ITNews/FavouriteNewsController.h
@@ -8,8 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FavouriteNewsController : UIViewController
+@interface FavouriteNewsController : UITableViewController
 
 
 @end
-
--- ITNews/ITNews/FavouriteNewsController.m
+++ ITNews/ITNews/FavouriteNewsController.m
@@ -7,21 +7,127 @@
 //
 
 #import "FavouriteNewsController.h"
+#import "CoreDataHelper.h"
+#import "News.h"
+#import "ArticleUITableViewCell.h"
+
+#define kBgQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
 
 @interface FavouriteNewsController ()
 
+@property(nonatomic, strong) CoreDataHelper* cdHelper;
+
 @end
 
-@implementation FavouriteNewsController
+@implementation FavouriteNewsController {
+    NSArray *fetchedObjects;
+}
+
+static NSString *const EntityName = @"News";
+
+static NSString *cellIdentifier = @"ArticleUITableViewCell";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UINib* nib = [UINib nibWithNibName:cellIdentifier bundle:nil];
+    [self.tableView registerNib:nib forCellReuseIdentifier:cellIdentifier];
+    
+    [self loadData];
+    
+//    //ADDING
+//    News* news1 = [NSEntityDescription insertNewObjectForEntityForName:EntityName inManagedObjectContext:_cdHelper.context];
+//    news1.title = @"Test 1";
+//    news1.content = @"Content 1";
+//    news1.author = @"Author 1";
+//    news1.thumbUrl = @"https://images-na.ssl-images-amazon.com/images/G/01/srrichar/laptop_bg_acer.jpg";
+//    news1.videoUrl = @"https://www.youtube.com/watch?v=bEJLv7IN7UE";
+//    
+//    
+//    News* news2 = [NSEntityDescription insertNewObjectForEntityForName:EntityName inManagedObjectContext:_cdHelper.context];
+//    
+//    [_cdHelper.context insertObject:news1];
+//    [_cdHelper.context insertObject:news2];
+//    news2.title = @"Test 2";
+//    news2.content = @"Content 2";
+//    news2.author = @"Author 2";
+//    news2.thumbUrl = @"https://images-na.ssl-images-amazon.com/images/G/01/srrichar/laptop_bg_acer.jpg";
+//    news2.videoUrl = @"https://www.youtube.com/watch?v=bEJLv7IN7UE";
+//    
+//    [self.cdHelper saveContext];
+    
+//    //DELETE:
+//    
+//    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:EntityName];
+//    NSArray *fetchedObjects = [_cdHelper.context executeFetchRequest:request error:nil];
+//    for (News *currentNews in fetchedObjects) {
+//        NSLog(@"Deleting Object '%@'", currentNews.title);
+//        [_cdHelper.context deleteObject:currentNews];
+//    }
+    
+    
+    //step 2: fetch this data
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return fetchedObjects.count;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    ArticleUITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    //
+    //    if (cell == nil) {
+    //        cell = [[ArticleUITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier] ;
+    //    }
+    
+    News *newsObject = [fetchedObjects objectAtIndex:indexPath.row];
+    UIImageView *thumb = cell.thumbLabel;
+    [thumb setImage: [UIImage imageNamed:@"globe.png"]];  // todo: change pic
+    
+    dispatch_async(kBgQueue, ^{
+        NSData *imgData = [NSData dataWithContentsOfURL:[NSURL URLWithString: newsObject.thumbUrl]];
+        if (imgData) {
+            UIImage *image = [UIImage imageWithData:imgData];
+            if (image) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [thumb setImage: image];                });
+            }
+        }
+    });
+    
+    cell.titleLabel.text = newsObject.title;
+    cell.contentLabel.text = newsObject.content;
+    
+    return cell;
+}
+
+-(void) loadData {
+    // Do any additional setup after loading the view, typically from a nib.
+    //step 1: Insert some data
+    _cdHelper = [[CoreDataHelper alloc] init];
+    [_cdHelper setupCoreData];
+    
+    // FETHICNG:
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:EntityName];
+    fetchedObjects = [_cdHelper.context executeFetchRequest:request error:nil];
+    [self.tableView reloadData];
+    
+//    for (News *currentNews in fetchedObjects) {
+//        NSLog(@"Fetched Object = %@", currentNews.title);
+//    }
 }
 
 @end
