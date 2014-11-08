7af837b9c6c372b63a4037527fb7987c9039ed3a
Георгис Андонис <lovechguy@gmail.com>
2014-11-07 17:18:51 -0800

    get data from weather api

--- ITNews/ITNews/WeatherViewController.m
+++ ITNews/ITNews/WeatherViewController.m
@@ -8,16 +8,82 @@
 
 #import "WeatherViewController.h"
 
-@interface WeatherViewController ()
+@interface WeatherViewController ()<NSURLConnectionDelegate>
 
 @end
 
-@implementation WeatherViewController
+@implementation WeatherViewController {
+    NSString* url;
+    NSString* dayOfWeek;
+    NSString* weatherIconUrl;
+    NSString* weatherInfo;
+    NSString* fullCity;
+    
+    NSMutableData* responseData;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    NSLog(@"Weather controller loaded");
+    [self loadData];
+    
+    
 }
+
+
+-(void) loadData{
+    url = @"http://api.wunderground.com/api/7904905845b78b09/conditions/forecast/alert/q/17.382042000000000000,78.48172729999999000.json";
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
+    
+    [request setHTTPMethod:@"GET"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-type"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    
+    [NSURLConnection connectionWithRequest:request delegate: self];
+}
+
+-(void) connection:(NSURLRequest*) request didReceiveData:(NSData *)data {
+
+  
+    NSDictionary* json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        if(json == nil){
+            NSLog(@"json is nil");
+            [self loadData];
+        } else {
+    
+        
+    NSDictionary* res = [json objectForKey: @"current_observation"];
+    
+    // full location
+    NSDictionary* location = [res objectForKey:@"display_location"];
+    
+    // forecast info
+    NSDictionary* forecast = [json objectForKey:@"forecast"];
+    NSDictionary* txtForecast = [forecast objectForKey:@"txt_forecast"];
+    NSArray* forecastDay = [txtForecast objectForKey:@"forecastday"];
+    
+    // day of week
+    dayOfWeek =[[forecastDay objectAtIndex:0] objectForKey:@"title"];
+    NSLog(@"Day: %@",dayOfWeek);
+    
+    // weather icon
+    weatherIconUrl =[[forecastDay objectAtIndex:0] objectForKey:@"icon_url"];
+    NSLog(@"Icon: %@",weatherIconUrl);
+    
+    // weather info
+    weatherInfo =[[forecastDay objectAtIndex:0] objectForKey:@"fcttext_metric"];
+    NSLog(@"Weather: %@",weatherInfo);
+    
+    // full location
+    fullCity = [location objectForKey:@"full"];
+    NSLog(@"%@",fullCity);
+    }
+    
+
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
